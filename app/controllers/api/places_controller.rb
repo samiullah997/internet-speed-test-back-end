f837d134ef5501e2660b016326624ceb8a4c4b89
samiullah997 <samiullahk997@gmail.com>
2023-03-24 21:47:11 +0500

    solve rubocop errors

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -1,28 +1,29 @@
 module Api
- class PlacesController < ApplicationController
+  class PlacesController < ApplicationController
     def index
-        places = Place.all.map do |place|
-            {
-                name: place.name,
-                city: place.city,
-                most_recent_download_speed: most_recent_download_speed(place),
-                most_recent_download_unit: most_recent_download_unit(place),
-                number_of_meaurments: number_of_meaurments(place)
-            }
-            render(json:{places: places})
-        end
+      places = Place.all.map do |place|
+        {
+          name: place.name,
+          city: place.city,
+          most_recent_download_speed: most_recent_download_speed(place),
+          most_recent_download_unit: most_recent_download_unit(place),
+          number_of_meaurments: number_of_meaurments(place)
+        }
+
+        render(json: { places: })
+      end
     end
 
     def most_recent_download_speed(place)
-        place.internet_speeds.order("created_at").last.download_speed
+      place.internet_speeds.order('created_at').last.download_speed
     end
 
     def most_recent_download_unit(place)
-        place.internet_speeds.order("created_at").last.download_units
+      place.internet_speeds.order('created_at').last.download_units
     end
 
     def number_of_meaurments(place)
-        place.internet_speeds.count
+      place.internet_speeds.count
     end
- end    
+  end
 end

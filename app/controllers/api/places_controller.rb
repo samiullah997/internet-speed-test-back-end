d4d07c070a1ea5ecc1a8a91eafd307dc7136d6b6
samiullah997 <samiullahk997@gmail.com>
2023-04-12 23:07:13 +0500

    fix bugs in PlacesController

--- app/controllers/api/places_controller.rb
+++ app/controllers/api/places_controller.rb
@@ -6,23 +6,22 @@ module Api
           name: place.name,
           city: place.city,
           most_recent_download_speed: most_recent_download_speed(place),
-          most_recent_download_unit: most_recent_download_unit(place),
-          number_of_meaurments: number_of_meaurments(place)
+          most_recent_download_units: most_recent_download_units(place),
+          number_of_measurments: number_of_measurments(place)
         }
-
-        render(json: { places: })
       end
+      render(json: { places: })
     end
 
     def most_recent_download_speed(place)
-      place.internet_speeds.order('created_at').last.download_speed
+      place.internet_speeds.order('created_at').last&.download_speed
     end
 
-    def most_recent_download_unit(place)
-      place.internet_speeds.order('created_at').last.download_units
+    def most_recent_download_units(place)
+      place.internet_speeds.order('created_at').last&.download_units
     end
 
-    def number_of_meaurments(place)
+    def number_of_measurments(place)
       place.internet_speeds.count
     end
   end
